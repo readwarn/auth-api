bce677365da3aac352bc0bf03f11310c74c1283a
Yusuf Rilwan <akinyusuf5@gmail.com>
2020-09-21 20:40:09 +0100

    hosted my client on heroku

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(passport.session());
 const passportSetup=require('./config/oauthStrategy');
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', 'https://hungry-shirley-b0e560.netlify.app');
+  res.header('Access-Control-Allow-Origin', 'https://warm-scrubland-49028.herokuapp.com');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
   next();
